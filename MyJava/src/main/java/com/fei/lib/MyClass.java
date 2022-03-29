80bb627920e014cad4f04865722221f89f050451
wangfei <929728742@qq.com>
2022-03-29 12:56:33 +0800

    索引条开发

--- MyJava/src/main/java/com/fei/lib/MyClass.java
+++ MyJava/src/main/java/com/fei/lib/MyClass.java
@@ -7,10 +7,16 @@ public class MyClass {
         String[] array = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
                 "o","p","q","r","s","t","u","v","w","x","y","z"};
         for (int i = 0; i < array.length; i++) {
-            if (i % 3 == 0 || i == 0 || i ==25) {
+            if (i % 4 == 0 || i == 0 || i == array.length - 1) {
                 System.out.println(array[i]);
             }
         }
+
+//        for (int i = 0; i < 26; i++) {
+//            if (i % 4 == 0 || i == 0 || i ==25) {
+//                System.out.println(array[i]);
+//            }
+//        }
     }
 
 }
